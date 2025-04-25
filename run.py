0619dae688a7c80df02b0a2c106b7d248805f57b
Jianyun Yang <24181422@student.uwa.edu.au>
2025-04-25 19:34:49 +0800

    Reset host address in run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ application = create_app()
 
 if __name__ == '__main__':
     # set host and port for the application
-    application.run(debug=True, host='0.0.0.0', port=5000)
+    application.run(debug=True, host='127.0.0.1', port=5000)
